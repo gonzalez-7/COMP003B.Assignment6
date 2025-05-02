7da4b83d4e230ff15d357716a732bd140793e2d4
gonzalez-7 <20agonzalezp@gmail.com>
2025-05-02 13:20:14 -0700

    Created 3 classes in the models folder -Book.cs -Author.cs -BookAuthors.cs

---
+++ COMP003B.Assignment6/Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace COMP003B.Assignment6.Models
+{
+	public class Author
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public ICollection<BookAuthor> BookAuthors { get; set; }
+	}
+}
---
+++ COMP003B.Assignment6/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace COMP003B.Assignment6.Models
+{
+	public class Book
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public ICollection<BookAuthor> BookAuthors { get; set; }
+	}
+}
